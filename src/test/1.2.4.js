e2dd9878faafe3347768e81ec841f774fe2ec72f
damikael <michele.damico@agid.gov.it>
2023-05-09 19:11:03 +0200

    feat: add test 1.2.4

---
+++ src/test/1.2.4.js
@@ -0,0 +1,58 @@
+const TestMetadata = require('../server/lib/test/TestMetadata.js');
+
+const jwt_decode = require('../server/node_modules/jwt-decode');
+const validator = require('../server/node_modules/validator');
+const axios = require('../server/node_modules/axios');
+const jose = require('../server/node_modules/node-jose');
+
+
+class Test_1_2_4 extends TestMetadata {
+
+    constructor(metadata) {
+        super(metadata);
+        this.num = "1.2.4";
+        this.description = "The signature of the JWS document MUST be valid";
+        this.validation = "automatic";
+    }
+
+    async exec() {
+        super.exec();
+
+        if(this.metadata.type!='federation') {
+            this.notes = "N/A (document is not provided as openid-federation)";
+            return true;
+        }
+        
+        let returnedDocument = this.metadata.entity_statement;
+        
+        if (!validator.isJWT(returnedDocument)) {
+            this.notes = returnedDocument;
+            throw 'returned document is not a valid JWT';
+        }
+    
+        let jwks = (await axios.get(this.metadata.configuration.jwks_uri)).data;
+    
+        if (jwks.keys == null || jwks.keys == '') {
+            this.notes = jwks;
+            throw 'JWKS not found';
+        }
+    
+        let keystore = jose.JWK.createKeyStore();
+        for (let k in jwks.keys) {
+            await keystore.add(jwks.keys[k]);
+        }
+    
+        let returnedDocumentVerified = await jose.JWS.createVerify(keystore).verify(
+            returnedDocument
+        );
+
+        this.notes = returnedDocument;
+
+        if (!returnedDocumentVerified) {
+            throw 'document not verifiable';
+        }
+    }
+
+}
+
+module.exports = Test_1_2_4
