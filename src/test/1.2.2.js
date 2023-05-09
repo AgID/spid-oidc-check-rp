b0896637307b48599dedbb2bf1d7a30c14fb50dd
damikael <michele.damico@agid.gov.it>
2023-05-09 18:25:58 +0200

    feat: add test 1.2.2

---
+++ src/test/1.2.2.js
@@ -0,0 +1,34 @@
+const TestMetadata = require('../server/lib/test/TestMetadata.js');
+const axios = require('../server/node_modules/axios');
+const validator = require('../server/node_modules/validator');
+
+class Test_1_2_2 extends TestMetadata {
+
+    constructor(metadata) {
+        super(metadata);
+        this.num = "1.2.2";
+        this.description = "The document MUST be returned as a valid JWS document";
+        this.validation = "automatic";
+    }
+
+    async exec() {
+        super.exec();
+        
+        if(this.metadata.type!='federation') {
+            this.notes = "N/A (document is not provided as openid-federation)";
+            return true;
+        }
+
+        let response = await axios.get(this.metadata.url);
+        this.notes = response.data;
+        
+        if(!validator.isJWT(response.data)) {
+            throw new Error("The document is not a valid JWT");
+        } else {
+            return true;
+        }
+    }
+
+}
+
+module.exports = Test_1_2_2
