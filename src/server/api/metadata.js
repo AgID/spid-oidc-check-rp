cbdf5bdb962b50b28690f37e86317f33ce98f3c0
Glauco <37829079+rglauco@users.noreply.github.com>
2023-10-22 15:04:04 +0200

    fix: basepath in metadata.js

--- src/server/api/metadata.js
+++ src/server/api/metadata.js
@@ -6,7 +6,7 @@ const jwt_decode = require('jwt-decode');
 const Utility = require('../lib/utils');
 const config_dir = require('../../config/dir.json');
 const config_test = require("../../config/test.json");
-
+const config_op = require("../../config/op.json");
 
 module.exports = function(app, checkAuthorisation, database) {
 
@@ -81,7 +81,7 @@ module.exports = function(app, checkAuthorisation, database) {
     */
 
     // download metadata 
-    app.post("/api/metadata/:type/download", function(req, res) {
+    app.post(config_op.basepath+"api/metadata/:type/download", function(req, res) {
 
         // check if apikey is correct
         let authorisation = checkAuthorisation(req);
@@ -181,7 +181,7 @@ module.exports = function(app, checkAuthorisation, database) {
     });
 
     // execute test for metadata
-    app.get("/api/metadata/check/:testcase", async function(req, res) {
+    app.get(config_op.basepath+"api/metadata/check/:testcase", async function(req, res) {
 
         // check if apikey is correct
         let authorisation = checkAuthorisation(req);
@@ -244,7 +244,7 @@ module.exports = function(app, checkAuthorisation, database) {
     });
 
     // return last validation from store
-    app.get("/api/metadata/lastcheck/:testcase", function(req, res) {
+    app.get(config_op.basepath+"api/metadata/lastcheck/:testcase", function(req, res) {
 
         // check if apikey is correct
         let authorisation = checkAuthorisation(req);
@@ -328,7 +328,7 @@ module.exports = function(app, checkAuthorisation, database) {
     */
 
     // set test for metadata
-    app.patch("/api/metadata/:testcase/:test", async function(req, res) {
+    app.patch(config_op.basepath+"api/metadata/:testcase/:test", async function(req, res) {
 
         // check if apikey is correct
         let authorisation = checkAuthorisation(req);
